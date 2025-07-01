9a7ed76039703e4f5df5a68ae2a616a42fe18262
Dieb <clapisdave@gamil.com>
2025-07-01 08:41:02 +0800

    Add contact.tsx

---
+++ resources/js/pages/contact.tsx
@@ -0,0 +1,9 @@
+// resources/js/pages/about/about.tsx
+
+export default function Contact() {
+    return (
+        <>
+            <h1 className="text-blue-500">contact</h1>
+        </>
+    );
+}
